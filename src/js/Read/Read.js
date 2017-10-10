86291ba426dac2312b7bd193029bdb12fbfc6eae
kzh <kzh@mit.edu>
2017-10-10 01:08:17 -0400

    Change to scrollable anchor

--- src/js/Read/Read.js
+++ src/js/Read/Read.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+import ScrollableAnchor from 'react-scrollable-anchor';
+
 import Scroll from 'react-scroll';
 import { scroller } from 'react-scroll';
 import ReactMarkdown from 'react-markdown';
@@ -57,38 +59,28 @@ export default class Read extends Component {
 		return (
 			<div className="read-page">
 				<div className="sidebar">
-	        <Link activeClass="active" to="test1" spy={true} smooth={true} offset={50} duration={500} onSetActive={this.handleSetActive}>
-	          Test 1
-	        </Link>
-	        <Link activeClass="active" to="test2" spy={true} smooth={true} offset={50} duration={500} >
-	          Test 2
-	        </Link>
-	        <Link activeClass="active" to="test3" spy={true} smooth={true} offset={50} duration={500} >
-	          Test 3
-	        </Link> 
-	        <Link activeClass="active" to="part1Chapter2" spy={true} smooth={true} offset={50} duration={500} >
-	          Part 1 Chapter 2
-	        </Link>
-	        <Link activeClass="active" to="part1Chapter3" spy={true} smooth={true} offset={50} duration={500} >
-	          Part 1 Chapter 3
-	        </Link>        
+	        <a href='#prefaceAboutTheAuthor'>About The Author</a>
+	        <a href='#prefaceChapter1'>Chapter 1</a>
+	        <a href='#prefaceChapter2'>Chapter 2</a>
+	        <a href='#part1Chapter2'>Chapter 2</a>
+	        <a href='#part1Chapter3'>Chapter 3</a>
 	      </div>
 	      <div className="read-content">    
-	        <Element name="test1" className="element">
+	        <ScrollableAnchor id={ 'prefaceAboutTheAuthor' }>
 	       	  <ReactMarkdown source={ prefaceAboutTheAuthor } />
-	        </Element>
-	        <Element name="test2" className="element">
+	        </ScrollableAnchor>
+	        <ScrollableAnchor id={ 'prefaceChapter1' }>
 	       	  <ReactMarkdown source={ prefaceChapter1 } />  
-	       	</Element>
-	       	<Element name="test3" className="element">
+	       	</ScrollableAnchor>
+	       	<ScrollableAnchor id={ 'prefaceChapter2' }>
 	        	<ReactMarkdown source={ prefaceChapter2 } />  
-	        </Element>
-	        <Element name="part1Chapter2" className="element">
+	        </ScrollableAnchor>
+	        <ScrollableAnchor id={ 'part1Chapter2' }>
 	       	  <ReactMarkdown source={ part1Chapter2 } />
-	        </Element>
-	        <Element name="part1Chapter3" className="element">
+	        </ScrollableAnchor>
+	        <ScrollableAnchor id={ 'part1Chapter3' }>
 	       	  <ReactMarkdown source={ part1Chapter3 } />  
-	       	</Element>
+	       	</ScrollableAnchor>
 	       </div> 
 			</div>
 		)
